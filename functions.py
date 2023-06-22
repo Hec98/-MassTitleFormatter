33ce75f4ae6c75f6a168fc47cfcca087cd41bf9c
hecdelatorre <hector982015@gmail.com>
2023-06-21 23:49:58 -0600

    Quit colored

--- functions.py
+++ functions.py
@@ -1,16 +1,10 @@
 from bullet import Input, colors, YesNo, Bullet
-from colored import fg, attr
 from os import listdir, rename
 from os.path import isdir, isfile
 from getpass import getuser
 
-colorRed = fg('#CC0000')
-colorGreen = fg('#73D216')
-colorBlue = fg('#338CFF')
-res = attr('reset')
-
 def err():
-    print(f'{colorRed}Wrong data{res}')
+    print('Wrong data')
     exit()
 
 def validateDir(directory): 
@@ -18,7 +12,7 @@ def validateDir(directory):
     else: return directory 
 
 def enterDirectory():
-    directory = Input(f'{colorGreen}Enter the directory {res}', default = f"/home/{getuser()}/Downloads", word_color = colors.foreground["yellow"]).launch()
+    directory = Input('Enter the directory', default = f"/home/{getuser()}/Downloads", word_color = colors.foreground["yellow"]).launch()
     directory = validateDir(directory)
     return directory
 
@@ -40,24 +34,24 @@ def traverseDirectory(directory):
     _, index = dir_file.launch()
     dir_file = True if index == 0 else False
 
-    print(f'Current name {colorBlue}::{res} New name')
+    print('Current name :: New name')
     for file in directoryArray: 
         if dir_file is True:
             if  isdir(f'{directory}/{file}'): 
-                print(f'{file} {colorRed}::{res} {file.title()}')
+                print(f'{file} :: {file.title()}')
                 directorys.append(file)
         else: 
             if  isfile(f'{directory}/{file}'): 
-                print(f'{file} {colorRed}::{res} {file.title()}')
+                print(f'{file} :: {file.title()}')
                 directorys.append(file)
 
     return directorys, dir_file
 
 def renameDirectory(directory, directorys, dir_file):
     dir_file = 'folders' if dir_file is True else 'files'
-    renameQuestion = YesNo(f'{colorGreen}Do you want to rename the {dir_file}? {res}', default = 'n', word_color = colors.foreground["yellow"]).launch()
+    renameQuestion = YesNo(f'Do you want to rename the {dir_file}?', default = 'n', word_color = colors.foreground["yellow"]).launch()
 
     if renameQuestion: 
         for name in directorys: rename(f'{directory}/{name}', f'{directory}/{name.title()}')
-        print(f'{colorBlue}Renamed directories{res}')
-    else: print(f'{colorRed}Directories not renamed{res}')
+        print('Renamed directories')
+    else: print('Directories not renamed')
